2a435a8e8634800838ddd5decde44588ef45e236
Adil <adil@bangthetable.com>
2021-05-16 14:56:19 +0530

    refactor(main): update ui and how we handle data

--- main.js
+++ main.js
@@ -1,24 +1,41 @@
-let paragraph = document.querySelector('h1');
+const quotesContainer = document.querySelector("main");
+const loadNewQuoteButton = document.querySelector("button");
 
-window.addEventListener('load', () => {
-    fetchQuotes();
-    registerSW();
-})
+const readQuotes = async () => {
+  let res = await fetch("./quotes.json");
 
-async function fetchQuotes() {
-    const res = await fetch('http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote');
-    const json = await res.json();
+  if (!res.ok) {
+    quotesContainer.innerHTML = `
+      <h1>Uh oh.</h1>
+      <strong>Something weird happened. Keep calm and try again 😇</strong>
+    `;
 
-    paragraph.innerHTML = json.starWarsQuote;
-}
+    loadNewQuoteButton.style.display = "none";
+    return;
+  }
 
-async function registerSW() {
-    if('serviceWorker' in navigator) {
-        try {
-            await navigator.serviceWorker.register('./sw.js');
-        }
-        catch(e) {
-            console.log('SW registration failed!');
-        }
+  res = await res.json();
+  const { quote, episode } = res[Math.floor(Math.random() * res.length)];
+
+  quotesContainer.innerHTML = `
+    <h1>${quote}</h1>
+    <em><strong>-- ${episode}</strong></em>
+  `;
+};
+
+const registerSW = async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      await navigator.serviceWorker.register("./sw.js");
+    } catch (e) {
+      console.error("SW registration failed!");
     }
-}
+  }
+};
+
+window.addEventListener("load", () => {
+  readQuotes();
+  registerSW();
+});
+
+loadNewQuoteButton.addEventListener("click", readQuotes);
