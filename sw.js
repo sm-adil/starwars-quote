06f5bda337271527957a16c621c486dcd1608487
Adil <adil@bangthetable.com>
2021-05-16 14:53:51 +0530

    refactor(sw.js): re-write sw in es6 syntax

--- sw.js
+++ sw.js
@@ -1,50 +1,48 @@
-const cacheName = 'quote-v1';
+const cacheName = "quote-v1";
 const staticAssets = [
-    './',
-    './index.html',
-    './main.css',
-    './main.js',
-    './manifest.webmanifest'
+  "./",
+  "./index.html",
+  "./main.css",
+  "./main.js",
+  "./manifest.webmanifest",
 ];
 
-self.addEventListener('install', async e => {
-    const cache = await caches.open(cacheName);
-    await cache.addAll(staticAssets);
-    return self.skipWaiting();
+self.addEventListener("install", async () => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-    self.clients.claim();
+self.addEventListener("activate", () => {
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', async e => {
-    const req = e.request;
-    const url = new URL(req.url);
+self.addEventListener("fetch", async (e) => {
+  const req = e.request;
+  const url = new URL(req.url);
 
-    if (url.origin === location.origin) {
-        e.respondWith(cacheFirst(req));
-    }
-    else {
-        e.respondWith(networkAndCache(req));
-    }
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
 });
 
-async function cacheFirst(req) {
-    const cache = await caches.open(cacheName);
-    const cached = await cache.match(req); 
-    return cached || fetch(req);
-}
+const cacheFirst = async (req) => {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+};
 
-async function networkAndCache(req) {
-    const cache = await caches.open(cacheName);
+const networkAndCache = async (req) => {
+  const cache = await caches.open(cacheName);
 
-    try {
-        const fresh = await fetch(req);
-        await cache.put(req, fresh.clone());
-        return fresh;
-    }
-    catch(e) {
-        const cached = await cache.match(req);
-        return cached;
-    }
-}
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+};
